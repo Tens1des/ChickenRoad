0a5fe3ddc4c86f4835b718866dd0aaa2b0f27898
Tens1de <romankotov3213@gmail.com>
2026-08-06 22:59:33 +0300

    Initial Commit

---
+++ ChikenRoad/ChikenRoadApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChikenRoadApp.swift
+//  ChikenRoad
+//
+//  Created by dad on 06.08.2026.
+//
+
+import SwiftUI
+
+@main
+struct ChikenRoadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
